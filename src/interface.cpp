123f75724043653b31349f989738d12b01e25ac7
MiSan Kaku <50670906+MiroKaku@users.noreply.github.com>
2021-03-13 21:00:20 +0800

    feat: empty

---
+++ src/interface.cpp
@@ -0,0 +1,11 @@
+// Copyright 2021 The Tapirus-Team Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+
+#include "interface.h"
+
+int add(int a, int b)
+{
+    return a + b;
+}
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+// Copyright 2021 The Tapirus-Team Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <Windows.h>
+#include <libbase.h>
+
+
+int main(int /*argc*/, char* /*argv*/[])
+{
+    return 0;
+}
