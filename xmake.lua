49e9bd09f8d2d2350342b63e54bc3b5754960a8a
luciouskami <realheiluo@gmail.com>
2022-06-11 20:10:37 +0800

    update xmake

--- xmake.lua
+++ xmake.lua
@@ -7,7 +7,7 @@ add_rules("plugin.vsxmake.autoupdate")
 
 set_project("libbase")
 add_rules("mode.debug", "mode.release")
-set_runtimes("MD")
+set_runtimes("MT")
 set_warnings("allextra", "error")
 set_languages("c11", "cxx17")
 add_includedirs(os.scriptdir())
